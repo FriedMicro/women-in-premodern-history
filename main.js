5be45ea8970b07272d738d788d4d2d1b58f43db6
Lucas Smith <friedmicrochips@gmail.com>
2017-05-01 13:38:48 -0400

    Complete main page.

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log("This source code is available under the GPL available at: https://www.gnu.org/licenses/gpl-3.0.en.html.");
+
+var navHeight = $(".row-75").height();
+$(".row-fourth").height(navHeight);
