52e0016544ecda2ee3fb37ad77a48c393bb8a4c8
Lucas Smith <friedmicrochips@gmail.com>
2017-05-01 20:55:24 -0400

    Finsh intro and Cleopatra page.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 console.log("This source code is available under the GPL available at: https://www.gnu.org/licenses/gpl-3.0.en.html.");
 
 var navHeight = $(".row-75").height();
-$(".row-fourth").height(navHeight);
+
+var currentNavHeight = $(".row-fourth").height();
+
+if(navHeight > currentNavHeight){
+    $(".row-fourth").height(navHeight);
+}
